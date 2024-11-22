3f60f8c4a0740b47be9b6838a952e7a40d2b4071
Gold Edem Hogan <edge37@outlook.com>
2024-11-22 16:26:02 +0100

    .

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use fastembedserver::{embed, embed_verses, process_json_file};
 use serde::{Deserialize, Serialize};
+use serde_json::json;
 use tokio::task;
 use warp::{reply::WithStatus, Filter, Reply};
 
@@ -31,83 +32,77 @@ struct VecEmbedding {
     index: usize,
 }
 
-async fn embed_verses_f() {
-    for row in process_json_file("t_ylt.json").unwrap() {
-        let res = reqwest::Client::new().put(format!("{}/collections/i/points?wait=true", std::env::var("QDRANT_URL").unwrap())).bearer_auth(std::env::var("QDRANT_KEY").unwrap()).body(format!(r#"{{points: [{{"id":"{}", "payload": {{"b": {}, "c": {}, "v": {}}}, "vector": {}, }}]}}"#, row.i, row.b, row.c, row.v, serde_json::to_string(&embed(&row.t).unwrap()).unwrap())).send().await.unwrap();
+async fn embed_verses_f() -> anyhow::Result<()> {
+    for row in process_json_file("t_ylt.json")? {
+        let res = reqwest::Client::new().put(format!("{}/collections/i/points?wait=true", std::env::var("QDRANT_URL")?)).bearer_auth(std::env::var("QDRANT_KEY")?).body(format!(r#"{{points: [{{"id":"{}", "payload": {{"b": {}, "c": {}, "v": {}}}, "vector": {}, }}]}}"#, row.i, row.b, row.c, row.v, serde_json::to_string(&embed(&row.t)?)?)).send().await?;
         println!("{} res: {:#?}", row.i, res);
     }
+    Ok(())
 }
 
 async fn embed_verses_handler() -> impl Reply {
-    task::spawn(embed_verses_f());
+    task::spawn(async {
+        embed_verses_f().await.unwrap();
+    });
     warp::reply::with_status("", warp::http::StatusCode::OK)
 }
 
 #[shuttle_runtime::main]
 async fn warp() -> shuttle_warp::ShuttleWarp<(impl Reply,)> {
+    dotenv::dotenv().ok();
     let embed_verses_route = warp::get()
         .and(warp::path!("embed_verses"))
         .then(embed_verses_handler);
+
+    // maybe buffer routes
+
     let json_route = warp::post()
         .and(warp::path!("embeddings"))
         .and(warp::body::json())
-        .map(|input: Input| {
-            let embedding = embed(&input.input).unwrap_or_default();
-            let response = ApiResponse {
-                object: "list",
-                data: vec![VecEmbedding {
-                    object: "embedding",
-                    embedding,
-                    index: 0,
-                }],
-                model: "text-embedding-ada-002",
-            };
-            warp::reply::json(&response)
-        });
-
-    let json_buffer_route = warp::post()
-        .and(warp::path!("embeddings"))
-        .and(warp::header("b"))
-        .and(warp::body::json())
-        .map(|_: String, input: Input| {
-            let embedding = embed(&input.input).unwrap_or_default();
-            let response = ApiResponse {
-                object: "list",
-                data: vec![VecEmbedding {
-                    object: "embedding",
-                    embedding,
-                    index: 0,
-                }],
-                model: "mxbai-embed-large-v1",
-            };
-            warp::reply::json(&response)
+        .map(|i: Input| match embed(&i.input) {
+            Ok(embedding) => {
+                let response = ApiResponse {
+                    object: "list",
+                    data: vec![VecEmbedding {
+                        object: "embedding",
+                        embedding,
+                        index: 0,
+                    }],
+                    model: "mxbai-embed-large-v1",
+                };
+                warp::reply::with_status(warp::reply::json(&response), warp::http::StatusCode::OK)
+            }
+            Err(e) => warp::reply::with_status(
+                warp::reply::json(&e.to_string()),
+                warp::http::StatusCode::INTERNAL_SERVER_ERROR,
+            ),
         });
 
     let json_plain_route = warp::path::end()
         .and(warp::post())
         .and(warp::body::json())
-        .map(|input: Input| warp::reply::json(&embed(&input.input).unwrap_or_default()));
+        .map(|i: Input| match embed(&i.input) {
+            Ok(e) => warp::reply::with_status(warp::reply::json(&e), warp::http::StatusCode::OK),
+            Err(e) => warp::reply::with_status(
+                warp::reply::json(&e.to_string()),
+                warp::http::StatusCode::INTERNAL_SERVER_ERROR,
+            ),
+        });
 
-    let query_route =
-        warp::get()
-            .and(warp::query::query::<QueryInput>())
-            .map(|query_input: QueryInput| {
-                warp::reply::json(&embed(&query_input.q).unwrap_or_default())
-            });
-
-    let query_buffer_route = warp::get()
-        .and(warp::header("b"))
+    let query_route = warp::get()
         .and(warp::query::query::<QueryInput>())
-        .map(|_: String, query_input: QueryInput| {
-            warp::reply::json(&embed(&query_input.q).unwrap_or_default())
+        .map(|q: QueryInput| match embed(&q.q) {
+            Ok(e) => warp::reply::with_status(warp::reply::json(&e), warp::http::StatusCode::OK),
+            Err(e) => warp::reply::with_status(
+                warp::reply::json(&e.to_string()),
+                warp::http::StatusCode::INTERNAL_SERVER_ERROR,
+            ),
         });
 
     Ok(json_route
         .or(embed_verses_route)
-        .or(json_buffer_route)
         .or(json_plain_route)
         .or(query_route)
-        .or(query_buffer_route)
         .boxed()
         .into())
 }
